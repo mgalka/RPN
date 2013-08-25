59a4086efa550c5d5560ca2e08b35140e8dfe52c
Michal Galka <galka.michal@gmail.com>
2013-08-26 01:07:27 +0200

    Adding infix expression string to list conversion

--- RPN.py
+++ RPN.py
@@ -26,7 +26,7 @@ class RPNOperator(object):
                          Higher number means higher priority.
         @type number_of_operands: int
         @param number_of_operands: The number of operands that operator needs.
-        @type call: callback
+        @type call: function
         @param call: A function that performs an operation on the operands
                      and returns a value.
                      If it's not defined the RPNCalculator won't be able
@@ -43,7 +43,10 @@ class RPNOperator(object):
     def __repr__(self):
         return "RPNOperator('%s')" % self.symbol
 
-    def calculate(self, *args):
+    def __call__(self, *args, **kwargs):
+        self.calculate(*args, **kwargs)
+
+    def calculate(self, *args, **kwargs):
         if self.call:
             return self.call(*args)
         else:
@@ -53,8 +56,9 @@ class RPNOperator(object):
 class RPNParser(object):
     """
     A parser which is able to create a RPNExpression object.
-    It needs to have operators defined (as a list of RPNOperator objects)
-    and parentheses symbols defined.
+    It needs to have operators (as a list of RPNOperator objects)
+    and parenthesis symbols defined. It should be used to generate
+    RPNExpressions which can be stored and/or evaluated later on.
     """
     def __init__(self, operators=None,
                  open_parenthesis='(', close_parenthesis=')'):
@@ -62,7 +66,8 @@ class RPNParser(object):
         initializes the parser.
         @type operators: list
         @param operators: list of RPNOperator objects.
-                          These operators will be used in parsing process
+                          These are used to determine what is an operator in the
+                          expression.
         @type open_parenthesis: string
         @param open_parenthesis: a symbol that will be interpreted as
                                  a left (opening) parenthesis. default '('
@@ -72,6 +77,51 @@ class RPNParser(object):
         self.open_parenthesis = open_parenthesis
         self.close_parenthesis = close_parenthesis
         self.operators = operators[:]
+
+    def _infix_to_list(self, infix_expression):
+        '''
+        This method changes infix_expression to the list to help parsing
+        by generate_rpm_expression. It's not meant to be used from outside.
+        It creates a list where every symbol/literal, operator or parenthesis are
+        a separate element in order to rpm expression generating method
+        @type infix_expression: str
+        @param infix_expression: Infix expression string
+        '''
+        pos = 0
+        expr_list = list()
+        slice_begin = 0
+        #Get characters on which we should stop when parsing
+        first_chars = [operator.symbol[0] for operator in self.operators]
+        first_chars.append(self.open_parenthesis[0])
+        first_chars.append(self.close_parenthesis[0])
+        #map objects with their names first characters
+        obj_to_stop = dict((char,list()) for char in set(first_chars))
+        map(lambda x: obj_to_stop[x.symbol[0]].append(x), self.operators)
+        obj_to_stop[self.open_parenthesis[0]].append(self.open_parenthesis)
+        obj_to_stop[self.close_parenthesis[0]].append(self.close_parenthesis)
+        while pos < len(infix_expression):
+            if infix_expression[pos] in obj_to_stop:
+                objects = obj_to_stop[infix_expression[pos]]
+                for obj in objects:
+                    if isinstance(obj, RPNOperator):
+                        length = len(obj.symbol)
+                        symbol = obj.symbol
+                    elif isinstance(obj, basestring):
+                        length = len(obj)
+                        symbol = obj
+                    if infix_expression[pos:pos + length] == symbol:
+                        if slice_begin < pos:
+                            expr_list.append(infix_expression[slice_begin: pos])
+                        expr_list.append(infix_expression[pos: pos + length])
+                        pos += length
+                        slice_begin = pos
+                        break
+                    else:
+                        pos += 1
+            else:
+                pos += 1
+        expr_list.append(infix_expression[slice_begin:])
+        return expr_list
 
     def check_parentheses(self, infix_expression):
         counter = 0
@@ -85,21 +135,30 @@ class RPNParser(object):
         if counter:
             raise ParenthesisMismatchError
 
-    def generateRPNExpression(self, infix_expression):
+    def generate_rpn_expression(self, infix_expression):
         """
         parses the expression in infix notation. It needs properly defined
         operators and parenthesis (@see: __init__).
         @type infix_expression: str
         @param infix_expression: infix expression to be parsed
-        @rtype: RPNExpression
+        @type: RPNExpression
         @return: a RPNExpression object that is ready to be evaluated
                  (provided that operators are properly defined).
         """
+        #remove white characters
+        infix_expression = ''.join(infix_expression.split())
         #TODO: optimize, check, test
         self.check_parentheses(infix_expression)
         stack = []
         output = RPNExpression()
-        for atom in infix_expression:
+        infix_expr_list = self._infix_to_list(infix_expression)
+        for atom in infix_expr_list:
+            print "ATOM: " + atom
+            print "STACK: " + str(stack)
+            print "OUTPUT: " + str(output)
+            print "OPERATORS: " + str(self.operators)
+            print "EXPR LIST: " + str(infix_expr_list)
+            print 5 * '='
             if atom == self.open_parenthesis:
                 stack.append(atom)
             elif atom == self.close_parenthesis:
@@ -131,3 +190,6 @@ class RPNParser(object):
 class RPNExpression(list):
     def evaluate(self):
         pass
+
+    def __str__(self):
+        return ' '.join(self)
