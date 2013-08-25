14ab8f10b4a4a02d4e6fc0a60118e62f7ab34032
Michal Galka <galka.michal@gmail.com>
2013-08-26 01:08:09 +0200

    New tests for infix string to list conversion

--- test.py
+++ test.py
@@ -28,8 +28,14 @@ class TestInfixConversionToRPN(unittest.TestCase):
                           RPN.RPNOperator("*", priority=2, number_of_operands=2, call=multiply),
                           RPN.RPNOperator("/", priority=2, number_of_operands=2, call=divide)
                           ]
+        self.long_operators = [
+                          RPN.RPNOperator("++", priority=1, number_of_operands=2, call=add),
+                          RPN.RPNOperator("---", priority=1, number_of_operands=2, call=substract),
+                          RPN.RPNOperator("****", priority=2, number_of_operands=2, call=multiply),
+                          RPN.RPNOperator("///", priority=2, number_of_operands=2, call=divide)
+                          ]
 
-    def testExpressionWithTheSamePriorities(self):
+    def testExpressionWithOneOperator(self):
         infix_expression = "2+2"
         parser = RPN.RPNParser(self.operators, open_parenthesis='(', close_parenthesis=')')
         rpn_expr = parser.generate_rpn_expression(infix_expression)
@@ -42,6 +48,36 @@ class TestInfixConversionToRPN(unittest.TestCase):
         rpn_expr = parser.generate_rpn_expression(infix_expression)
         print rpn_expr
         self.assertEqual(str(rpn_expr), "25 2 + 132 -")
+
+    def testInfixExpressionToList(self):
+        infix_expression = '(12*4)+5'
+        expected_result = ['(','12','*','4',')','+','5']
+        parser = RPN.RPNParser(self.operators, open_parenthesis='(', close_parenthesis=')')
+        self.assertEqual(parser._infix_to_list(infix_expression), expected_result)
+
+    def testInfixExpressionToListWithLongParenthesisSymbols(self):
+        infix_expression = '((12*4)))+5'
+        expected_result = ['((','12','*','4',')))','+','5']
+        parser = RPN.RPNParser(self.operators, open_parenthesis='((', close_parenthesis=')))')
+        self.assertEqual(parser._infix_to_list(infix_expression), expected_result)
+
+    def testInfixExpressionToListWithLongOperatorSymbols(self):
+        infix_expression = '(12****4)++5'
+        expected_result = ['(','12','****','4',')','++','5']
+        parser = RPN.RPNParser(self.long_operators, open_parenthesis='(', close_parenthesis=')')
+        self.assertEqual(parser._infix_to_list(infix_expression), expected_result)
+
+    def testInfixExpressionToListWithLiteralsBeginingLikeOperator(self):
+        infix_expression = '(12****4)+5'
+        expected_result = ['(','12','****','4',')','+5']
+        parser = RPN.RPNParser(self.long_operators, open_parenthesis='(', close_parenthesis=')')
+        self.assertEqual(parser._infix_to_list(infix_expression), expected_result)
+
+    def testInfixExpressionToListWithLongOperatorSymbolsAndLongParenthesis(self):
+        infix_expression = '((12****4))++5'
+        expected_result = ['((','12','****','4','))','++','5']
+        parser = RPN.RPNParser(self.long_operators, open_parenthesis='((', close_parenthesis='))')
+        self.assertEqual(parser._infix_to_list(infix_expression), expected_result)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
